31f436e3df951269cbf1d155cd396ae8e176c7fc
Rajesh mule <rajeshsmule@gmail.com>
2020-03-19 14:52:22 +0530

    verify token in auth with 'Token {token}'

--- modules/auth.js
+++ modules/auth.js
@@ -6,9 +6,20 @@ exports.generateJWT = async user => {
   return token;
 };
 
+function getTokenFromHeader(req) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Token"
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  }
+
+  return null;
+}
+
 exports.validateJWT = async (req, res, next) => {
   try {
-    let token = "" || req.headers["authorization"];
+    let token = getTokenFromHeader(req);
     var payload = await jwt.verify(token, process.env.SECRET);
     req.user = payload;
     req.user.token = token;
@@ -20,7 +31,7 @@ exports.validateJWT = async (req, res, next) => {
 
 exports.optionalValidateJWT = async (req, res, next) => {
   try {
-    let token = "" || req.headers["authorization"];
+    let token = getTokenFromHeader(req);
     if (token) {
       var payload = await jwt.verify(token, process.env.SECRET);
       req.user = payload;
